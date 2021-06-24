f26d6454af6fe1db34a7ec01200f060a859bff28
mrsyed <mrsyed@yahoo.com>
2021-06-24 13:19:53 -0700

    The scratch file opens a txt file and converts to csv file. Numeric strings are removed using re or regex and only strings are kept

--- scratch.py
+++ scratch.py
@@ -1,3 +1,5 @@
+import csv, re
+
 # try:
 #     file = open('as.txt', 'r')
 #
@@ -23,20 +25,35 @@
 #     make_pie(4)
 # except IndexError as error_message:
 #     print('fruit pie')
-facebook_posts = [
-    {'Likes': 21, 'Comments': 2},
-    {'Likes': 13, 'Comments': 2, 'Shares': 1},
-    {'Likes': 33, 'Comments': 8, 'Shares': 3},
-    {'Comments': 4, 'Shares': 2},
-    {'Comments': 1, 'Shares': 1},
-    {'Likes': 19, 'Comments': 3}
-]
+# facebook_posts = [
+#     {'Likes': 21, 'Comments': 2},
+#     {'Likes': 13, 'Comments': 2, 'Shares': 1},
+#     {'Likes': 33, 'Comments': 8, 'Shares': 3},
+#     {'Comments': 4, 'Shares': 2},
+#     {'Comments': 1, 'Shares': 1},
+#     {'Likes': 19, 'Comments': 3}
+# ]
+#
+# total_likes = 0
+# for post in facebook_posts:
+#     try:
+#         total_likes = total_likes + post['Likes']
+#     except KeyError:
+#         total_likes +=0
+#
+# print(total_likes)
 
-total_likes = 0
-for post in facebook_posts:
-    try:
-        total_likes = total_likes + post['Likes']
-    except KeyError:
-        total_likes +=0
+# This opens a text file and converts to csv file, all list items removes the numeric values
+with open('fr_50k.txt') as csv_file:
+    pattern = r'[0-9]'
 
-print(total_likes)
+    fr_list = [re.sub(pattern, '', line).replace("\n","") for line in csv_file]
+    print(fr_list)
+
+with open('fr_50k.csv', 'w') as write_csv:
+    csv_writer = csv.writer(write_csv, lineterminator='\n')
+    for fr in fr_list:
+        csv_writer.writerow([fr])
+
+# for l in fr_list:
+#     print(l)
